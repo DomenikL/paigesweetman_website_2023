54f7166d38df0f7c1c6377ff127f02cf7fe6683b
DomenikL <domenik@langbein.name>
2024-10-14 19:41:03 +1300

    adding like button with backend

---
+++ SQL/likes.sql
@@ -0,0 +1,6 @@
+create table likes (
+    artpiece_id int, 
+    user_id varchar(255),
+	primary key (artpiece_id, user_id)
+);
+
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const { v4: uuidv4 } = require("uuid");
 
 const db = require("./db").createPool();
+
+app.use(cookieParser());
 
 app.use(express.static("public"));
 
@@ -13,6 +17,11 @@ app.get("/api/artpieces", async (req, res) => {
 });
 
 app.get("/api/artpiece", async (req, res) => {
+  let uuid = req.cookies.userid;
+  if (!uuid) {
+    uuid = uuidv4();
+    res.cookie("userid", uuid);
+  }
   const idParam = req.query.id;
   if (idParam) {
     if (idParam.match(/^[0-9]+$/)) {
@@ -26,6 +35,94 @@ app.get("/api/artpiece", async (req, res) => {
       } else {
         res.status(404).json({ message: "Art piece not found" });
       }
+    } else {
+      res.status(400).json({ error: "ID parameter must be numeric" });
+    }
+  } else {
+    res.status(400).json({ error: "Missing id parameter" });
+  }
+});
+
+app.get("/api/likes", async (req, res) => {
+  let uuid = req.cookies.userid;
+  if (!uuid) {
+    uuid = uuidv4();
+    res.cookie("userid", uuid);
+  }
+  const idParam = req.query.id;
+  if (idParam) {
+    if (idParam.match(/^[0-9]+$/)) {
+      const totalcountresult = await db.query(
+        "select count(*) as like_count from likes where artpiece_id = ?",
+        [idParam]
+      );
+      const usercountresult = await db.query(
+        "select count(*) as like_count from likes where artpiece_id = ? and user_id = ?",
+        [idParam, uuid]
+      );
+      res.json({
+        like_count: totalcountresult[0].like_count,
+        user_like: usercountresult[0].like_count,
+      });
+      return;
+    } else {
+      res.status(400).json({ error: "ID parameter must be numeric" });
+    }
+  } else {
+    res.status(400).json({ error: "Missing id parameter" });
+  }
+});
+
+app.put("/api/likes", async (req, res) => {
+  let uuid = req.cookies.userid;
+  if (!uuid) {
+    uuid = uuidv4();
+    res.cookie("userid", uuid);
+  }
+  const idParam = req.query.id;
+  if (idParam) {
+    if (idParam.match(/^[0-9]+$/)) {
+      const usercountresult = await db.query(
+        "select count(*) as like_count from likes where artpiece_id = ? and user_id = ?",
+        [idParam, uuid]
+      );
+      if (usercountresult[0].like_count === 0) {
+        await db.query(
+          "insert into likes (artpiece_id, user_id) values (?, ?)",
+          [idParam, uuid]
+        );
+      }
+      res.send();
+      return;
+    } else {
+      res.status(400).json({ error: "ID parameter must be numeric" });
+    }
+  } else {
+    res.status(400).json({ error: "Missing id parameter" });
+  }
+});
+
+app.delete("/api/likes", async (req, res) => {
+  let uuid = req.cookies.userid;
+  if (!uuid) {
+    uuid = uuidv4();
+    res.cookie("userid", uuid);
+  }
+  const idParam = req.query.id;
+  if (idParam) {
+    if (idParam.match(/^[0-9]+$/)) {
+      const usercountresult = await db.query(
+        "select count(*) as like_count from likes where artpiece_id = ? and user_id = ?",
+        [idParam, uuid]
+      );
+      if (usercountresult[0].like_count === 1) {
+        await db.query(
+          "delete from likes where artpiece_id = ? and user_id = ?",
+          [idParam, uuid]
+        );
+      }
+      res.send();
+      return;
     } else {
       res.status(400).json({ error: "ID parameter must be numeric" });
     }
